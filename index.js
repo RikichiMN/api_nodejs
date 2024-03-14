0c604bc9a4ac49f9d6f90f1b5434a7bf59ec12a3
RikichiMN <henrique.nepomuceno@etec.sp.gov.br>
2024-03-14 20:40:11 -0300

    aula 14/03

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const router = require('./routes/routes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
 // const porta = process.en.PORT || 3333;
 const porta = 3333;
