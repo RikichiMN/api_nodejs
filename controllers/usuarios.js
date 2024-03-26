12479be41d830d8468d47f41f4f5ace19411e228
RikichiMN <henrique.nepomuceno@etec.sp.gov.br>
2024-03-26 19:38:53 -0300

    26/03 19:38

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,62 +1,67 @@
-const { json, response } = require('express');
-const db = require('../database/connection');
+const { json } = require('express'); 
+const db = require('../database/connection'); 
 
 module.exports = {
     async listarUsuarios(request, response) {
-    try {
-        return response.status(200).json({
-            sucesso : true,
-            mensagem: 'Lista de usuários.',
-            dados: null
-        });
-    } catch (error) {
-        return response.status(500).json({
-        sucesso: false,
-        mensagem: `Erro na requisição. \n ${error}`,
-        dados: null
-        });
-    }
-}, async cadastrarUsuarios(request, response) {
-    try {
-        return response.status(200).json({
-            sucesso : true,
-            mensagem: 'Cadastro de usuários.',
-            dados: null
-        });
-    } catch (error) {
-        return response.status(500).json({
-        sucesso: false,
-        mensagem: `Erro na requisição. \n ${error}`,
-        dados: null
-        });
-    }
-},async editarUsuarios(request, response) {
-    try {
-        return response.status(200).json({
-            sucesso : true,
-            mensagem: 'Editar usuários.',
-            dados: null
-        });
-    } catch (error) {
-        return response.status(500).json({
-        sucesso: false,
-        mensagem: `Erro na requisição. \n ${error}`,
-        dados: null
-        });
-    }
-},async apagarUsuarios(request, response) {
-    try {
-        return response.status(200).json({
-            sucesso : true,
-            mensagem: 'Apagar usuários.',
-            dados: null
-        });
-    } catch (error) {
-        return response.status(500).json({
-        sucesso: false,
-        mensagem: `Erro na requisição. \n ${error}`,
-        dados: null
-        });
-    }
-},
+        try {
+            // throw new Error('Eu causei o erro!');
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Lista de usuários.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: `Erro na requisição. -${error}`, 
+                dados: null
+            });
+        }
+    }, 
+    async cadastrarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Cadastro de usuários.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: `Erro na requisição. -${error}`, 
+                dados: null
+            });
+        }
+    }, 
+    async editarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Editar usuários.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: `Erro na requisição. -${error}`, 
+                dados: null
+            });
+        }
+    }, 
+    async apagarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Apagar usuários.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: `Erro na requisição. -${error}`, 
+                dados: null
+            });
+        }
+    }, 
 }
+
